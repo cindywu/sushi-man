e80ffb90a1b62f6d1a9ee5c74799ebc2e9647d6d
cindy <1177031+cindywu@users.noreply.github.com>
2023-03-03 10:21:31 -1000

    play

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Sushi from '../components/sushi'
+import {nanoid} from 'nanoid'
 
 type menuItem = {
+  id: string
   name: string
   price: number
   minimumCount: number
@@ -8,166 +10,199 @@ type menuItem = {
 
 const menu : menuItem[] = [
   {
+    id: nanoid(),
     name: "red dragon maki",
     price: 8,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "dynamite maki",
     price: 8,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "dragon maki",
     price: 8,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "shrimp tempura cal maki",
     price: 8,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "plate lunch",
     price: 12,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "gyoza (5pcs)",
     price: 5,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "shrimp tempura",
     price: 2,
     minimumCount: 2
   },
   {
+    id: nanoid(),
     name: "veggie tempura",
     price: 2,
     minimumCount: 2
   },
   {
+    id: nanoid(),
     name: "inari (5pcs)",
     price: 4.5,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "sesame ball (8pcs)",
     price: 5,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "chicken beef bento",
     price: 8,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "unagi bento",
     price: 8,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "garlic miso mayo salmon",
     price: 9,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "egg roll",
     price: 2.5,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "spicy crab roll",
     price: 3,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "spicy ahi natto roll",
     price: 3.5,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "unagi roll",
     price: 3,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "crab roll",
     price: 2.5,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "spam crab roll",
     price: 3.5,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "ahi roll",
     price: 3,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "natto roll",
     price: 2.5,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "tuna mayo roll",
     price: 2.5,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "natto ahi roll",
     price: 3.5,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "spam egg roll",
     price: 3,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "spicy ahi spam roll",
     price: 3.5,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "ebiko roll",
     price: 4,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "spam roll",
     price: 2.5,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "spicy ahi ebiko roll",
     price: 5,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "spicy ahi roll",
     price: 3,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "spicy ahi inari",
     price: 6,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "spicy crab inari",
     price: 6,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "crunchy bowl",
     price: 9,
     minimumCount: 1
   },
   {
+    id: nanoid(),
     name: "tempura bowl",
     price: 9,
     minimumCount: 1
--- components/sushi.tsx
+++ components/sushi.tsx
@@ -1,16 +1,120 @@
-import React from 'react'
+"use client"
+import React, { useState, useEffect, useRef } from 'react'
+import {nanoid} from 'nanoid'
 
 export default function Sushi({menu}: any) {
+  const [playing, setPlaying] = useState(false)
+
+  return (
+    <>
+      {!playing ?
+        <MenuPage
+          menu={menu}
+          setPlaying={setPlaying}
+          playing={playing}
+        />
+        :
+        <PlayPage
+          menu={menu}
+        />
+      }
+    </>
+  )
+}
+
+function PlayPage({menu}: any){
+  const [randomItem, setRandomItem] = useState(null)
+  const [showWrong, setShowWrong] = useState(false)
+  const [showTrue, setShowTrue] = useState(false)
+  const [randomIndex, setRandomIndex] = useState(Math.floor(Math.random() * menu.length))
+  const [score, setScore] = useState(0)
+  const [gameLog, setGameLog] = useState([])
+
+  useEffect(() => {
+    console.log({gameLog})
+
+  }, [gameLog])
+
+  useEffect(() => {
+    setRandomItem(menu[randomIndex])
+  },[])
+
+  useEffect(() => {
+    setRandomItem(menu[randomIndex])
+  }, [randomIndex])
+
+  const guessRef = useRef(null)
+
+  function checkIfGuessEqualsPrice(){
+    const guess = parseFloat(guessRef.current.value)
+    const price = randomItem.price
+
+    console.log({guess})
+    console.log({price})
+    if (guess !== price){
+      setShowWrong(true)
+      setShowTrue(false)
+      setGameLog([...gameLog, {id: nanoid(), name: randomItem.name, guess: guess, correct: false}])
+    } else {
+      setShowWrong(false)
+      setShowTrue(true)
+      setRandomIndex(Math.floor(Math.random() * menu.length))
+      console.log("i am in here")
+      setGameLog([...gameLog, {id: nanoid(), name: randomItem.name, guess: guess, correct: true}])
+      setScore(score + 1)
+      guessRef.current.value = null
+    }
+  }
+
+  return (
+    <>
+    <div className={'p-4 font-mono flex justify-between w-1/2 cursor-pointer'}><div>score: {score}</div><div className={'text-white hover:text-black'} onClick={() => setScore(0)}>reset</div></div>
+    <div className={'p-4 font-mono w-1/2 flex justify-between'}>
+      <div>{menu[randomIndex].name}</div>
+      <input
+        className={"border-2 w-24 px-2 font-mono outline-none"}
+        ref={guessRef}
+      ></input>
+      <button onClick={() => checkIfGuessEqualsPrice()}>enter</button>
+    </div>
+    {/* <div className={"p-4"}>
+      {showWrong && <div className={"text-rose-600 font-mono"}>wrong</div>}
+      {showTrue && <div className={"text-lime-600 font-mono"}>correct</div>}
+    </div> */}
+    <div className={'p-4 font-mono'}>
+      {gameLog.map((item: any) => {
+        return (
+          <div
+            key={item.id}
+            className={"flex justify-between w-1/2"}
+          >
+            <div>{item.name}</div>
+            <div className={`${item.correct ?
+              'text-lime-600' : 'text-rose-600'}`}>{item.guess}</div>
+          </div>
+        )
+      })}
+
+    </div>
+    </>
+  )
+}
+
+function MenuPage({menu, setPlaying, playing}:any){
   return (
     <div className={'p-4 font-mono w-1/2'}>
       <div className={'pb-4 flex justify-between'}>
         <div>welcome to your sushi shift</div>
-        <div className={'text-orange-600 hover:text-orange-700 cursor-pointer'}>start work</div>
+        <div
+          className={'text-orange-600 hover:text-orange-700 cursor-pointer'}
+          onClick={() => setPlaying(!playing)}
+        >
+          start work</div>
       </div>
       {menu.map((item: any) => {
         return (
           <div
-            key={item.name}
+            key={item.id}
             className={"flex justify-between"}
           >
             <div>{item.name}</div>
