12af5e2053a2c18ce12b883d55b18c447aeaab33
cindy <1177031+cindywu@users.noreply.github.com>
2023-03-03 09:21:37 -1000

    start work/

--- components/sushi.tsx
+++ components/sushi.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Sushi({menu}: any) {
   return (
     <div className={'p-4 font-mono w-1/2'}>
-      <div className={'pb-4'}>welcome to your shift at sushi man</div>
+      <div className={'pb-4 flex justify-between'}>
+        <div>welcome to your sushi shift</div>
+        <div className={'text-orange-600 hover:text-orange-700 cursor-pointer'}>start work</div>
+      </div>
       {menu.map((item: any) => {
         return (
           <div
